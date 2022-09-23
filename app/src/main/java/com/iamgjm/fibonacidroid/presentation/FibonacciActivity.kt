f6230fe7ede1a4947fe23f582095ac210186edd8
iamgjm <gjorgjimarkov@yahoo.com>
2022-09-23 16:18:16 +0200

    [] Initial commit

---
+++ app/src/main/java/com/iamgjm/fibonacidroid/presentation/FibonacciActivity.kt
@@ -0,0 +1,12 @@
+package com.iamgjm.fibonacidroid.presentation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.iamgjm.fibonacidroid.R
+
+class FibonacciActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
